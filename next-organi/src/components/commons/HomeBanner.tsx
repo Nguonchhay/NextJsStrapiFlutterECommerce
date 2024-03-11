739917667730c710e4d039abba84b9a0d2332e62
Nguonchhay Touch <nguonchhay.touch@gmail.com>
2024-03-11 17:12:49 +0700

    [Next] Home banner

--- next-organi/src/components/commons/HomeBanner.tsx
+++ next-organi/src/components/commons/HomeBanner.tsx
@@ -1,6 +1,7 @@
 import CategoryQuickView from "./CategoryQuickView";
 import QuickSearch from "./QuickSearch";
 import QuickContact from "./QuickContact";
+import Image from "next/image";
 
 export default function HomeBanner() {
   return (
@@ -8,11 +9,28 @@ export default function HomeBanner() {
       <div className="w-3/12">
         <CategoryQuickView />
       </div>
-      <div className="w-6/12">
-        <QuickSearch />
-      </div>
-      <div className="w-3/12">
-        <QuickContact />
+      <div className="w-9/12">
+        <div className="flex">
+            <div className="w-8/12">
+                <QuickSearch />
+            </div>
+            <div className="w-3/12">
+                <QuickContact />
+            </div>
+        </div>
+        <div className="relative mt-5">
+            <Image src="/images/banner.webp" width={750} height={380} alt="Banner"/>
+            <div className="absolute top-0 left-0">
+                <div className="flex flex-col justify-center w-[300px] h-[370px] pl-16">
+                    <span className="uppercase font-bold text-orange-400">Fruit Fresh</span>
+                    <h2 className="font-extrabold text-[2rem]">
+                        Vegetable <br/>100% Organic
+                    </h2>
+                    <p className="text-xs text-gray-600">Free Pickup and Delivery Available</p>
+                    <a href="#" className="w-max mt-7 uppercase text-sm font-extrabold text-white bg-orange-400 p-2 px-6">Shop Now</a>
+                </div>
+            </div>
+        </div>
       </div>
     </section>
   )
--- next-organi/src/components/commons/QuickSearch.tsx
+++ next-organi/src/components/commons/QuickSearch.tsx
@@ -1,5 +1,18 @@
 export default function QuickSearch() {
   return (
-    <div>QuickSearch</div>
+    <form className="w-[95%] inline-flex justify-between items-center border border-gray-200">
+      <div className="">
+        <select className="form-select border-0 font-bold">
+          <option value="all" defaultValue="all">All Categories</option>
+        </select>
+      </div>
+      <div className="text-gray-200">|</div>
+      <div className="">
+        <input type="text" className="form-input border-0" placeholder="What do you want?" />
+      </div>
+      <div className="">
+        <button type="submit" className="form-input border-0 text-white bg-orange-400">Search</button>
+      </div>
+    </form>
   )
 }
